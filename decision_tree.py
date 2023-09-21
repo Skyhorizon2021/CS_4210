9d80c58b2e2742ea0e34b109f05ab3dffab85612
Skyhorizon2021 <locnguyen02152021@gmail.com>
2023-09-21 02:17:33 -0700

    Update

--- decision_tree.py
+++ decision_tree.py
@@ -3,7 +3,7 @@
 # FILENAME: decision_tree.py
 # SPECIFICATION: Read input from CSV file and output ID3 decision tree
 # FOR: CS 4210- Assignment #1
-# TIME SPENT: how long it took you to complete the assignment
+# TIME SPENT: 2 hours
 #-----------------------------------------------------------*/
 
 #IMPORTANT NOTE: DO NOT USE ANY ADVANCED PYTHON LIBRARY TO COMPLETE THIS CODE SUCH AS numpy OR pandas. You have to work here only with standard
