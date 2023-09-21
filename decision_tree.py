ea4a6d05f34393977a3222dc83b19e1e85659a93
Skyhorizon2021 <locnguyen02152021@gmail.com>
2023-09-21 02:14:29 -0700

    Update decision_tree.py

--- decision_tree.py
+++ decision_tree.py
@@ -1,7 +1,7 @@
 #-------------------------------------------------------------------------
-# AUTHOR: your name
-# FILENAME: title of the source file
-# SPECIFICATION: description of the program
+# AUTHOR: Loc Nguyen
+# FILENAME: decision_tree.py
+# SPECIFICATION: Read input from CSV file and output ID3 decision tree
 # FOR: CS 4210- Assignment #1
 # TIME SPENT: how long it took you to complete the assignment
 #-----------------------------------------------------------*/
@@ -12,7 +12,7 @@
 #importing some Python libraries
 from sklearn import tree
 import matplotlib.pyplot as plt
-import csv
+import csv,copy
 db = []
 X = []
 Y = []
@@ -25,19 +25,61 @@ with open('contact_lens.csv', 'r') as csvfile:
          db.append (row)
          print(row)
 
+newdb = copy.deepcopy(db)
+
+
 #transform the original categorical training features into numbers and add to the 4D array X. For instance Young = 1, Prepresbyopic = 2, Presbyopic = 3
 # so X = [[1, 1, 1, 1], [2, 2, 2, 2], ...]]
 #--> add your Python code here
 # X =
+for i in range(len(newdb)):
+   for j in range(len(newdb[i])-1):
+      if j == 0:
+        match newdb[i][j]:
+          case "Young":
+            newdb[i][j] = 1
+          case "Prepresbyopic":
+            newdb[i][j] = 2
+          case "Presbyopic":
+            newdb[i][j] = 3
+      elif j == 1:
+        match newdb[i][j]:
+          case "Myope":
+            newdb[i][j] = 1
+          case "Hypermetrope":
+            newdb[i][j] = 2
+      if j == 2:
+        match newdb[i][j]:
+          case "Yes":
+            newdb[i][j] = 1
+          case "No":
+            newdb[i][j] = 2
+      if j == 3:
+        match newdb[i][j]:
+          case "Reduced":
+            newdb[i][j] = 1
+          case "Normal":
+            newdb[i][j] = 2
 
+X = newdb
+for i in range(len(X)):
+  del X[i][4]
+print(X)
 #transform the original categorical training classes into numbers and add to the vector Y. For instance Yes = 1, No = 2, so Y = [1, 1, 2, 2, ...]
 #--> addd your Python code here
 # Y =
+for i in range(len(db)):
+  Y.append(db[i][4])
+  if Y[i] == "Yes":
+    Y[i] = 1
+  elif Y[i] == "No":
+    Y[i] = 2
 
+print(Y)
 #fitting the decision tree to the data
-clf = tree.DecisionTreeClassifier(criterion = 'entropy')
-clf = clf.fit(X, Y)
+#clf = tree.DecisionTreeClassifier(criterion = 'entropy')
+#clf = clf.fit(X, Y)
 
 #plotting the decision tree
-tree.plot_tree(clf, feature_names=['Age', 'Spectacle', 'Astigmatism', 'Tear'], class_names=['Yes','No'], filled=True, rounded=True)
-plt.show()
+#tree.plot_tree(clf, feature_names=['Age', 'Spectacle', 'Astigmatism', 'Tear'], class_names=['Yes','No'], filled=True, rounded=True)
+#plt.show()
